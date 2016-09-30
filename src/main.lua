cee35f2f3ab08b8de47d424031c379330ca11316
Paul Liverman III <paul.liverman.iii@gmail.com>
2016-09-29 19:04:49 -0700

    working prototype

--- src/main.lua
+++ src/main.lua
@@ -4,34 +4,71 @@ local lk = love.keyboard
 -- planetary mass, radius, atmospheric height, gravitational constant
 local p_mass, p_radius, p_atmosphere, G = 100000, 100, 20, 1
 
+local function Ship()
+  local self = {}
+  self.x = 0
+  self.y = -p_radius
+  self.v = {x=0,y=0}        -- velocity
+  self.heading = -math.pi/2 -- pointing up
+  self.radius = 5 --TEMPORARY FOR LOVE.DRAW!
+  return self
+end
+
 local ships = {}
+local currentShip = Ship()
+table.insert(ships, currentShip)
+--TEMPORARY FOR ORBITAL START
+currentShip.y = currentShip.y - p_atmosphere - 13
+currentShip.v.x = 150
 
 local time, tick = 0, 1/30
 
-local function gravity(A, B)
-  local dx = A.x - B.x
-  local dy = A.y - B.y
-  local d2 = dx*dx + dy*dy
-  local Fg = G * A.mass * B.mass / d2
+local function gravity(A)
+  local d2 = A.x*A.x + A.y*A.y
+  local Fg = G * p_mass / d2
   -- apply force
-  local d = math.atan2(dy,dx)
-  local dcos = math.cos(d)
-  local dsin = math.sin(d)
-  A.v.x = A.v.x - (Fg/A.mass) * dcos
-  A.v.y = A.v.y - (Fg/A.mass) * dsin
-  B.v.x = B.v.x + (Fg/B.mass) * dcos
-  B.v.y = B.v.y + (Fg/B.mass) * dsin
+  local d = math.atan2(A.y, A.x)
+  A.v.x = A.v.x - Fg * math.cos(d)
+  A.v.y = A.v.y - Fg * math.sin(d)
+end
+
+local function collide(A)
+  d = math.sqrt(A.x*A.x + A.y*A.y)
+  if d <= p_radius then
+    -- need to stop acceleration
+    --TODO crash or live based on velocity at impact
+    A.v.x = 0
+    A.v.y = 0
+    -- need to move to surface and stay on the surface
+    local t = math.atan2(A.y, A.x)
+    A.x = A.x + (p_radius - d) * math.cos(t)
+    A.y = A.y + (p_radius - d) * math.sin(t)
+  end
+end
+
+-- this updates the angle relative to planet
+local function angle(A)
+  d = math.sqrt(A.x*A.x + A.y*A.y)
+  --TODO partial translation when in atmosphere
+  if d >= p_radius + p_atmosphere then
+    A.heading = math.atan2(A.y, A.x) + math.pi/2
+  end
 end
 
 local function update()
-  for i=1,#ships-1,2 do
-    for j=i+1,#ships do
-      gravity(ships[i], ships[j])
-    end
+  for i=1,#ships do
+    gravity(ships[i])
+    ships[i].x = ships[i].x + ships[i].v.x * tick
+    ships[i].y = ships[i].y + ships[i].v.y * tick
+    collide(ships[i])
+    angle(ships[i])
+    --TODO add drag calculation for in atmosphere
   end
-  for _, body in ipairs(ships) do
-    body.x = body.x + body.v.x * tick
-    body.y = body.y + body.v.y * tick
+
+  if lk.isDown("z") then
+    --TODO 20 should be modified based on distance
+    currentShip.v.x = currentShip.v.x + 20 * math.cos(currentShip.heading)
+    currentShip.v.y = currentShip.v.y + 20 * math.sin(currentShip.heading)
   end
 end
 
@@ -45,106 +82,17 @@ end
 
 function love.draw()
   lg.translate(lg.getWidth()/2, lg.getHeight()/2)
-  for _, body in ipairs(ships) do
-    lg.circle("fill", body.x, body.y, body.radius)
-  end
-end
-
-function love.keypressed(key)
-  if key == "escape" then
-    love.event.quit()
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
-local function Ship()
-  local self = {}
-  self.x = 0
-  self.y = -r
-  self.v = {x=0,y=0}  -- velocity
-  self.r = -math.pi/2 -- pointing up
-  return self
-end
-
-local ships = {}
-local currentShip = Ship()
-table.insert(ships, currentShip)
-
-function gravity(ship)
-  local d2 = ship.x*ship.x + ship.y*ship.y
-  local g = G * m / d2
-
-  local x = math.abs(ship.x) * g / (math.abs(ship.x)+math.abs(ship.y))
-  if ship.x < 0 then
-    ship.v.x = ship.v.x - x * 1/30
-  else
-    ship.v.x = ship.v.x + x * 1/30
-  end
-  if ship.y < 0 then
-    ship.v.y = ship.v.y + (g - x) * 1/30
-  else
-    ship.v.y = ship.v.y - (g - x) * 1/30
-  end
-end
-
-function accelerate(ship)
-  local a = 11 -- approximately 1m/s^2 higher than g
-  ship.v.x = ship.v.x + a*math.cos(ship.r)*1/30
-  ship.v.y = ship.v.y + a*math.sin(ship.r)*1/30
-end
-
-function collide(ship)
-  local d = math.sqrt(ship.x*ship.x + ship.y*ship.y)
-  if d <= r then
-    -- move ship radially to surface,
-    ship.x = ship.x + (r-d)*math.cos(math.atan2(ship.y, ship.x))
-    ship.y = ship.y + (r-d)*math.sin(math.atan2(ship.y, ship.x))
-    -- reset velocity in that direction to 0
-  end
-end
-
-function update()
-  for _, ship in pairs(ships) do
-    gravity(ship)
-    collide(ship)
-    ship.x = ship.x + ship.v.x
-    ship.y = ship.y + ship.v.y
-    ship.r = math.atan2(ship.y, ship.x) + math.pi/2
-  end
-
-  if lk.isDown("z") then
-    accelerate(currentShip)
-  end
-end
-
-local time, tick = 0, 1/30
-function love.update(dt)
-  time = time + dt
-  if time >= tick then
-    time = time - tick
-    update()
-  end
-end
-
-function love.draw()
-  lg.setColor(0, 255, 0, 255)
-  lg.circle("fill", lg.getWidth()/2, lg.getHeight()/2, r)
-  lg.setColor(0, 0, 100, 120)
-  lg.circle("fill", lg.getWidth()/2, lg.getHeight()/2, r+h)
 
   lg.setColor(200, 150, 100, 255)
-  for _, ship in pairs(ships) do
-    lg.rectangle("fill", ship.x - 2 + lg.getWidth()/2, ship.y - 2 + lg.getHeight()/2, 4, 4)
+  for i=1,#ships do
+    lg.circle("fill", ships[i].x, ships[i].y, ships[i].radius)
   end
+
+  lg.setColor(0, 255, 0, 255)
+  lg.circle("fill", 0, 0, p_radius)
+  --TODO improve atmosphere rendering
+  lg.setColor(0, 0, 100, 120)
+  lg.circle("fill", 0, 0, p_radius + p_atmosphere)
 end
 
 function love.keypressed(key)
